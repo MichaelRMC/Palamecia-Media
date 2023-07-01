3b829be002a912ba0227941179f23d0a90332252
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-07-01 12:39:40 -0400

    Add CSS Styling

--- app.js
+++ app.js
@@ -12,48 +12,60 @@ form.addEventListener("submit", (event) => {
   let inStock = event.target.inStock.value;
 });
 
-function getGameInfo(boxArt, name, genre, platform, price, inStock) {
- const li = document.createElement("li");
- li.innerHTML = boxArt
+function getWorldInfo(boxArt, name, genre, platform, price, inStock) {
+  if(boxArt) {
+		const div = document.createElement("div");
+		div.className = "boxArt";
+		li append(div, boxArt);
+	}	
+ 
+	if(name) {
+	  const h2 = document.createElement("h2")
+	  h2.textContent = name		
+	  li.append(h2)
+	}
+	if(genre) {
+	  const p = document.createElement("p")
+	   p.textContent = genre;
+	  li.append(p)
+	}
+	if(platform){
+	  const p = document.createElement("p")
+	  p.textContent = platform;
+	  li.append(p)
+	}
+	if (price) {
+	  const p = document.createElement("p")
+	  p.textContent = price;
+	  li.append(p)
+	}
+	if(inStock) {
+	  let stockButton = document.createElement("button")
+	  stockButton = inStock
+	  stockButton.addEventListener("click", (event) => {
+		  stockButton.classList.toggle ("in-stock")
+		  stockButton.classList.toggle ("not-in-stock")
 
-
- switch (form.value) {
-	case name:
-		const h2 = document.createElement("h2")
-		h2.textContent = name		
-		break;
-	case genre:
-		const p = document.createElement("p")
-		p.textContent = genre;
-		break;
-	case platform:
-		const p = document.createElement("p")
-		p.textContent = platform;
-		break;
-	case price:
-		const p = document.createElement("p")
-		p.textContent = price;
-		break;
-	case inStock:
-		let stockButton = document.createElement("button")
-		stockButton = inStock
-		stockButton.addEventListener("click", (event) => {
-			stockButton.classList.toggle ("in-stock")
-			stockButton.classList.toggle ("not-in-stock")
-
-			const className = stockButton.className
-			if (className == "in-stock") {
-				this.textContent = "In Stock"
+		  const className = stockButton.className
+		  if (className == "in-stock") {
+		      this.textContent = "In Stock"
 			} else {
-				this.textContent = "Not In Stock"
+			  this.textContent = "Not In Stock"
 			}
 		})
+	  li.append(stockButton)
+	}
 
-	default:
-		const remove = document.createElement("button")
-		remove.textContent = "Remove"
-		break;
+	const remove = document.createElement("button")
+	remove.textContent = "Remove"
+	li.append(remove)
+
+	remove.addEventListener("click", (event) => {
+	    li.remove()
+		})
  }
-		
 
+function createGalaxyMap(boxArt, name, genre, platform, price, inStock) {
+	const li = getWorldInfo(boxArt, name, genre, platform, price, inStock)
+	ul.prepend(li)
 }
