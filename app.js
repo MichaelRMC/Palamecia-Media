72043851f1f618fd6e94ea80a1e2be69c34c3e7d
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-06-30 21:15:12 -0400

    Update DOM Events and Functions

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const inventory = document.getElementById(".inventory");
-const form = do
-const submit = document.querySelector("submit");
-form.addEventListener("submit", (event) => {
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
 
-} )
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	let boxArt = event.target.imageUrl.value;
+	let name = event.target.name.value;
+	let genre = event.target.genre.value;
+	let platform = event.target.platform.value;
+	let price = event.target.price.value;
+	let inStock = event.target.inStock.value;
+
+	console.log(boxArt);
+	console.log(name);
+	console.log(genre);
+	console.log(platform);
+	console.log(price);
+	console.log(inStock);
+});
