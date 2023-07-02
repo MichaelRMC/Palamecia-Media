72bd5179ed1eb9e69ca5bef20babadbd1771f5ee
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-07-02 08:58:37 -0400

    Format Project

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const worldCard = document.querySelector("worldCard");
 const worldDetails = document.querySelector("worldDetails");
 const galaxyMap = document.querySelector("#galaxyMap");
 
-
 form.addEventListener("submit", (event) => {
   let boxArt = event.target.imageUrl.value;
   let name = event.target.name.value;
@@ -12,67 +11,67 @@ form.addEventListener("submit", (event) => {
   let price = event.target.price.value;
   let inStock = event.target.inStock.value;
 
-  createGalaxyMap( boxArt, name, genre, platform, price, inStock)
+  createGalaxyMap(boxArt, name, genre, platform, price, inStock);
 
-  form.reset()
+  form.reset();
 });
 
 function getWorldInfo(boxArt, name, genre, platform, price, inStock) {
-  if(boxArt) {
-		const img = document.createElement("img");
-		img.setAttribute(src, boxArt);
-	  worldCard.append(img);
-	}	
- 
-	if(name) {
-	  const h4 = document.createElement("h4")
-	  h4.innerHTML = name;
-	  worldDetails.append(h4);
-	}
-	if(genre) {
-	  const p = document.createElement("p")
-	   p.innerHTML = genre;
-	  worldDetails.append(p)
-	}
-	if(platform){
-	  const p = document.createElement("p")
-	  p.innerHTML = platform;
-	  worldDetails.append(p)
-	}
-	if (price) {
-	  const p = document.createElement("p")
-	  p.innerHTML = price;
-	  worldDetails.append(p)
-	}
-	if(inStock) {
-	  let stockButton = document.createElement("button")
-	  stockButton = inStock
-	  stockButton.addEventListener("click", (event) => {
-		  stockButton.classList.toggle ("in-stock")
-		  stockButton.classList.toggle ("not-in-stock")
+  if (boxArt) {
+    const img = document.createElement("img");
+    img.setAttribute(src, boxArt);
+    worldCard.append(img);
+  }
 
-		  const className = stockButton.className
-		  if (className == "in-stock") {
-		      this.innerHTML = "In Stock"
-			} else {
-			  this.innerHTML = "Not In Stock"
-			}
-		})
+  if (name) {
+    const h4 = document.createElement("h4");
+    h4.innerHTML = name;
+    worldDetails.append(h4);
+  }
+  if (genre) {
+    const p = document.createElement("p");
+    p.innerHTML = genre;
+    worldDetails.append(p);
+  }
+  if (platform) {
+    const p = document.createElement("p");
+    p.innerHTML = platform;
+    worldDetails.append(p);
+  }
+  if (price) {
+    const p = document.createElement("p");
+    p.innerHTML = price;
+    worldDetails.append(p);
+  }
+  if (inStock) {
+    let stockButton = document.createElement("button");
+    stockButton = inStock;
+    stockButton.addEventListener("click", (event) => {
+      stockButton.classList.toggle("in-stock");
+      stockButton.classList.toggle("not-in-stock");
 
-	  worldDetails.append(stockButton)
-	}
+      const className = stockButton.className;
+      if (className == "in-stock") {
+        this.innerHTML = "In Stock";
+      } else {
+        this.innerHTML = "Not In Stock";
+      }
+    });
 
-	const remove = document.createElement("button")
-	remove.innerHTML = "Remove"
-	galaxyMap.append(remove)
+    worldDetails.append(stockButton);
+  }
 
-	remove.addEventListener("click", (event) => {
-	    worldCard.remove()
-		return galaxyMap
-		})
- }
+  const remove = document.createElement("button");
+  remove.innerHTML = "Remove";
+  galaxyMap.append(remove);
+
+  remove.addEventListener("click", (event) => {
+    worldCard.remove();
+    return galaxyMap;
+  });
+}
 
 function createGalaxyMap(boxArt, name, genre, platform, price, inStock) {
-	const worldCard = getWorldInfo(boxArt, name, genre, platform, price, inStock)
-	galaxyMap.prepend(li)
+  const worldCard = getWorldInfo(boxArt, name, genre, platform, price, inStock);
+  galaxyMap.prepend(li);
 }
